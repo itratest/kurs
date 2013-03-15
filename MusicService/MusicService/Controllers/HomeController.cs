e8313dec21e6ffcaa8735675bf5ea1033e688083
KiselN <nikita_lk@yahoo.com>
2013-03-15 22:59:43 +0200

    fix upload

--- MusicService/MusicService/Controllers/HomeController.cs
+++ MusicService/MusicService/Controllers/HomeController.cs
@@ -34,30 +34,7 @@ namespace MusicService.Controllers
             return View();
         }
 
-        public ActionResult UploadFile(int? entityId) // optionally receive values specified with Html helper
-        {
-            // here we can send in some extra info to be included with the delete url 
-            var statuses = new List<ViewDataUploadFileResult>();
-            for (var i = 0; i < Request.Files.Count; i++)
-            {
-                var st = FileSaver.StoreFile(x =>
-                {
-                    x.File = Request.Files[i];
-                    //note how we are adding an additional value to be posted with delete request
-                    //and giving it the same value posted with upload
-                    x.DeleteUrl = Url.Action("DeleteFile", new { entityId = entityId });
-                    x.StorageDirectory = Server.MapPath("~/Content/uploads");
-                    x.UrlPrefix = "/Content/uploads";
-                });
-                CreateTrack(st.name,st.url);
-                statuses.Add(st);
-            }
-            //statuses contains all the uploaded files details (if error occurs the check error property is not null or empty)
-            //todo: add additional code to generate thumbnail for videos, associate files with entities etc
-            //adding thumbnail url for jquery file upload javascript plugin
-            //statuses.ForEach(x=>x.thumbnail_url=x.url+"?width=80&height=80"); // uses ImageResizer httpmodule to resize images from this url
-            return Json(statuses);
-        }
+       
 
         public void CreateTrack(string name, string fileUrl)
         {
